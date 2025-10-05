a2100b68a08c982e51161ee31b675d71b4cbf416
Afshin-a64 <a.ataei.n11@gmail.com>
2025-10-06 00:22:17 +0330

    create component

--- project-revert-number/src/main.jsx
+++ project-revert-number/src/main.jsx
@@ -1,12 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './style.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./style.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
+class UserNumber extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 20,
+    };
+  }
+
+  render() {
+    function decrement(num) {
+      if (num > 0) {
+        return num - 1
+      }else {
+        return 0
+      }
+      // return num > 0 ? num - 1 : 0;
+    }
+    setInterval(() => {
+      this.setState({
+        count: decrement(this.state.count),
+      });
+    }, 1000);
+
+    return (
+      <div className="container">
+        <p className="counter">number is reversing: {this.state.count}</p>
+      </div>
+    );
+  }
+}
+
+createRoot(document.getElementById("root")).render(<UserNumber />);
